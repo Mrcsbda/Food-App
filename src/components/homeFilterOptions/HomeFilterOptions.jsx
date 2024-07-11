6eaaf5ef274cb8e39436d93a6ebb809dca958ced
Mariana <macbedoya@gmail.com>
2024-07-11 09:27:42 -0500

    component rebuilding

---
+++ src/components/homeFilterOptions/HomeFilterOptions.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+const HomeFilterOptions = () => {
+  const [selectedOption, setSelectedOption] = useState("ALL");
+  const filterOptions = [
+    {
+      value: "ALL",
+    },
+    {
+      value: "FAST FOOD",
+      path: "./images/fast-food.png",
+    },
+    {
+      value: "HEALTHY",
+      path: "./images/healthy.png",
+    },
+    {
+      value: "BURGUER",
+      path: "./images/burguer.png",
+    },
+    {
+      value: "PIZZA",
+      path: "./images/pizza.png",
+    },
+    {
+      value: "CHICKEN",
+      path: "./images/chicken.png",
+    },
+  ];
+
+  const titleCase = (title) => {
+    return title[0].toUpperCase() + title.slice(1).toLowerCase();
+  };
+
+  const selectOption = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <Swiper
+      slidesPerView={2}
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      breakpoints={{
+        500: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+      }}
+      className="mySwiper"
+    >
+      {filterOptions.map((option, index) => {
+        return (
+          <SwiperSlide key={index} onClick={() => selectOption(option.value)}>
+            <section
+              className={`home__filter-option ${
+                option.value == selectedOption
+                  ? "home__filter-option--selected"
+                  : ""
+              }`}
+            >
+              {option?.path && (
+                <img
+                  src={option.path}
+                  alt={`${option.value} image`}
+                  className="home__filter-option--icon"
+                />
+              )}
+              <p className="home__filter-option--text">
+                {" "}
+                {titleCase(option.value)}
+              </p>
+            </section>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
+export default HomeFilterOptions;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,130 +1,14 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "./home.scss";
+import HomeCarrousel from "../../components/homeCarrousel/HomeCarrousel";
+import HomeFilterOptions from "../../components/homeFilterOptions/HomeFilterOptions";
 const Home = () => {
-  const selectedOption = "ALL";
-  const carrouselImages = [
-    {
-      name: "Slide 1",
-      path: "./images/slide-1.jpg",
-    },
-    {
-      name: "Slide 2",
-      path: "./images/slide-2.jpg",
-    },
-    {
-      name: "Slide 3",
-      path: "./images/slide-3.jpg",
-    },
-    {
-      name: "Slide 4",
-      path: "./images/slide-4.jpg",
-    },
-  ];
-  const filterOptions = [
-    {
-      value: "ALL",
-    },
-    {
-      value: "FAST FOOD",
-      path: "./images/fast-food.png",
-    },
-    {
-      value: "HEALTHY",
-      path: "./images/healthy.png",
-    },
-    {
-      value: "BURGUER",
-      path: "./images/burguer.png",
-    },
-    {
-      value: "PIZZA",
-      path: "./images/pizza.png",
-    },
-    {
-      value: "CHICKEN",
-      path: "./images/chicken.png",
-    },
-  ];
-
   return (
     <main className="home">
-      <section className="home__carrousel">
-        <Swiper
-          slidesPerView={2}
-          spaceBetween={10}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            500: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
-          }}
-          className="mySwiper"
-        >
-          {carrouselImages.map((image, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <img
-                  src={image.path}
-                  alt={image.name}
-                  className="home__carrousel-image"
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </section>
+      <HomeCarrousel />
       <h1 className="home__title">Restaurants</h1>
-      <Swiper
-        slidesPerView={2}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          500: {
-            slidesPerView: 3,
-            spaceBetween: 10,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 10,
-          },
-        }}
-        className="mySwiper"
-      >
-        {filterOptions.map((option, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <section
-                className={`home__filter-option ${
-                  option.value == selectedOption
-                    ? "home__filter-option--selected"
-                    : ""
-                }`}
-              >
-                {option?.path && (
-                  <img
-                    src={option.path}
-                    alt={`${option.value} image`}
-                    className="home__filter-option--icon"
-                  />
-                )}
-                <p className="home__filter-option--text"> {option.value}</p>
-              </section>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      <HomeFilterOptions />
     </main>
   );
 };
