231355732496db4b2bf0ee392f3c986d4352cc74
Mariana <macbedoya@gmail.com>
2024-07-27 11:24:52 -0500

    error page

--- src/pages/dishSearch/DishSearch.jsx
+++ src/pages/dishSearch/DishSearch.jsx
@@ -2,26 +2,34 @@ import React, { useEffect, useState } from "react";
 import "./dishSearch.scss";
 import SearchResult from "../../components/dishSearch/searchResult/SearchResult";
 import { getAllDishes } from "../../services/firebase/restaurants";
+import Loader from "../../components/loader/Loader";
+import Error from "../../components/error/Error";
 const DishSearch = () => {
   const [dishesInfo, setDishesInfo] = useState([]);
   const [copyDishesInfo, setCopyDishesInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     handleSearch();
   }, []);
 
   const handleSearch = async () => {
+    setLoading(true);
     const resp = await getAllDishes();
     if (resp.length > 0) {
       setDishesInfo(resp);
       setCopyDishesInfo(resp);
+      setLoading(false);
     } else {
-
+      setLoading(false);
+      setError(true);
     }
   };
 
   return (
     <main className="dish-search">
+      {loading && <Loader />}
       <section className="dish-search__search-container">
         <img
           className="dish-search__search-icon"
@@ -39,11 +47,15 @@ const DishSearch = () => {
           alt="delete icon"
         />
       </section>
-      <section className="dish-search__results-container">
-        {dishesInfo.map((dish, index) => {
-          return <SearchResult dishInfo={dish} key={index} />;
-        })}
-      </section>
+      {error ? (
+        <Error />
+      ) : (
+        <section className="dish-search__results-container">
+          {dishesInfo.map((dish, index) => {
+            return <SearchResult dishInfo={dish} key={index} />;
+          })}
+        </section>
+      )}
     </main>
   );
 };
